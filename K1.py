b362b1035a36a26cf56497ac4a7fcfc65f5c7f66
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2018-11-08 10:03:29 +0530

    Update K1.py

--- K1.py
+++ K1.py
@@ -204,3 +204,180 @@ plt.ylabel('Revenue value', fontsize=15) # ylabel and size of words
 plt.title("Revenue Value Distribution", fontsize=20) # Setting Title and fontsize
 
 plt.show()
+
+"""
+Skewness 
+Degree of distortion from the symmetrical bell curve or the normal distribution. It measures the lack of symmetry in data distribution.
+It differentiates extreme values in one versus the other tail. A symmetrical distribution will have a skewness of 0.
+
+Positive Skewness means when the tail on the right side of the distribution is longer or fatter. The mean and median will be greater than the mode.
+Negative Skewness is when the tail of the left side of the distribution is longer or fatter than the tail on the right side. The mean and median will be less than the mode.
+
+So, when is the skewness too much?
+The rule of thumb seems to be:
+If the skewness is between -0.5 and 0.5, the data are fairly symmetrical. 
+If the skewness is between -1 and -0.5(negatively skewed) or between 0.5 and 1(positively skewed), the data are moderately skewed.
+If the skewness is less than -1(negatively skewed) or greater than 1(positively skewed), the data are highly skewed.
+
+
+Kurtosis
+Kurtosis is all about the tails of the distribution — not the peakedness or flatness. It is used to describe the extreme values in one versus the other tail. 
+It is actually the measure of outliers present in the distribution.
+
+High kurtosis in a data set is an indicator that data has heavy tails or outliers. If there is a high kurtosis, then, we need to investigate why do we have so many outliers. It indicates a lot of things, maybe wrong data entry or other things. Investigate! 
+Low kurtosis in a data set is an indicator that data has light tails or lack of outliers. If we get low kurtosis(too good to be true), then also we need to investigate and trim the dataset of unwanted results
+
+"""
+
+print('Excess kurtosis of normal distribution (should be 0): {}'.format(
+    kurtosis(df_train[df_train['totals.transactionRevenue'] > 0]["totals.transactionRevenue"])))
+print( 'Skewness of normal distribution (should be 0): {}'.format(
+    skew((df_train[df_train['totals.transactionRevenue'] > 0]["totals.transactionRevenue"]))))
+
+
+def CalcOutliers(df_num): 
+    '''
+    
+    Leonardo Ferreira 20/10/2018
+    Set a numerical value and it will calculate the upper, lower and total number of outliers
+    It will print a lot of statistics of the numerical feature that you set on input
+    
+    '''
+    # calculating mean and std of the array
+    data_mean, data_std = np.mean(df_num), np.std(df_num)
+
+    # seting the cut line to both higher and lower values
+    # You can change this value
+    cut = data_std * 3
+
+    #Calculating the higher and lower cut values
+    lower, upper = data_mean - cut, data_mean + cut
+
+    # creating an array of lower, higher and total outlier values 
+    outliers_lower = [x for x in df_num if x < lower]
+    outliers_higher = [x for x in df_num if x > upper]
+    outliers_total = [x for x in df_num if x < lower or x > upper]
+
+    # array without outlier values
+    outliers_removed = [x for x in df_num if x > lower and x < upper]
+    
+    print('Identified lowest outliers: %d' % len(outliers_lower)) # printing total number of values in lower cut of outliers
+    print('Identified upper outliers: %d' % len(outliers_higher)) # printing total number of values in higher cut of outliers
+    print('Identified outliers: %d' % len(outliers_total)) # printing total number of values outliers of both sides
+    print('Non-outlier observations: %d' % len(outliers_removed)) # printing total number of non outlier values
+    print("Total percentual of Outliers: ", round((len(outliers_total) / len(outliers_removed) )*100, 4)) # Percentual of outliers in points
+    
+    return
+
+
+CalcOutliers(df_train['totals.transactionRevenue']) # Call 
+CalcOutliers(df_train['totals.pageviews']) # Call 
+
+
+#Device Browsers
+# the top 10 of browsers represent % of total
+print("Percentual of Browser usage: ")
+print(df_train['device.browser'].value_counts()[:7] ) # printing the top 7 percentage of browsers
+
+# seting the graph size
+plt.figure(figsize=(14,6))
+
+# Let explore the browser used by users
+sns.countplot(df_train[df_train['device.browser']\
+                       .isin(df_train['device.browser']\
+                             .value_counts()[:10].index.values)]['device.browser'], palette="hls") # It's a module to count the category's
+plt.title("TOP 10 Most Frequent Browsers", fontsize=20) # Adding Title and seting the size
+plt.xlabel("Browser Names", fontsize=16) # Adding x label and seting the size
+plt.ylabel("Count", fontsize=16) # Adding y label and seting the size
+plt.xticks(rotation=45) # Adjust the xticks, rotating the labels
+
+plt.show() #use plt.show to render the graph that we did above
+
+#What if we cross the Revenue and Browser
+plt.figure(figsize=(13,6)) #figure size
+
+#It's another way to plot our data. using a variable that contains the plot parameters
+g1 = sns.boxenplot(x='device.browser', y='totals.transactionRevenue', 
+                   data=df_train[(df_train['device.browser'].isin((df_train['device.browser'].value_counts()[:10].index.values))) &
+                                  df_train['totals.transactionRevenue'] > 0])
+g1.set_title('Browsers Name by Transactions Revenue', fontsize=20) # title and fontsize
+g1.set_xticklabels(g1.get_xticklabels(),rotation=45) # It's the way to rotate the xticks when we use variable to our graphs
+g1.set_xlabel('Device Names', fontsize=18) # Xlabel
+g1.set_ylabel('Trans Revenue(log) Dist', fontsize=18) #Ylabel
+
+plt.show()
+
+
+#Channel Grouping - The channel via which the user came to the Store.
+# the top 10 of browsers represent % of total
+print("Percentual of Channel Grouping used: ")
+print((df_train['channelGrouping'].value_counts()[:5])) # printing the top 7 percentage of browsers
+
+# seting the graph size
+plt.figure(figsize=(14,7))
+
+# let explore the browser used by users
+sns.countplot(df_train["channelGrouping"], palette="hls") # It's a module to count the category's
+plt.title("Channel Grouping Count", fontsize=20) # seting the title size
+plt.xlabel("Channel Grouping Name", fontsize=18) # seting the x label size
+plt.ylabel("Count", fontsize=18) # seting the y label size
+
+plt.show() #use plt.show to render the graph that we did above
+
+#Crossing Channel Grouping x Browsers
+## I will use the crosstab to explore two categorical values
+
+# At index I will use set my variable that I want analyse and cross by another
+crosstab_eda = pd.crosstab(index=df_train['channelGrouping'], normalize=True,
+                           # at this line, I am using the isin to select just the top 5 of browsers
+                           columns=df_train[df_train['device.browser'].isin(df_train['device.browser']\
+                                                                            .value_counts()[:5].index.values)]['device.browser'])
+# Ploting the crosstab that we did above
+crosstab_eda.plot(kind="bar",    # select the bar to plot the count of categoricals
+                 figsize=(14,7), # adjusting the size of graphs
+                 stacked=True)   # code to unstack 
+plt.title("Channel Grouping % for which Browser", fontsize=20) # seting the title size
+plt.xlabel("The Channel Grouping Name", fontsize=18) # seting the x label size
+plt.ylabel("Count", fontsize=18) # seting the y label size
+plt.xticks(rotation=0)
+plt.show() # rendering
+
+#Operational System
+# the top 5 of browsers represent % of total
+print("Percentual of Operational System: ")
+print(df_train['device.operatingSystem'].value_counts()[:5]) # printing the top 7 percentage of browsers
+
+# seting the graph size
+plt.figure(figsize=(14,7))
+
+# let explore the browser used by users
+sns.countplot(df_train["device.operatingSystem"], palette="hls") # It's a module to count the category's
+plt.title("Operational System used Count", fontsize=20) # seting the title size
+plt.xlabel("Operational System Name", fontsize=16) # seting the x label size
+plt.ylabel("OS Count", fontsize=16) # seting the y label size
+plt.xticks(rotation=45) # Adjust the xticks, rotating the labels
+
+plt.show() #use plt.show to render the graph that we did above
+
+#most used brower by Operational System
+# At index I will use isin to substitute the loop and get just the values with more than 1%
+crosstab_eda = pd.crosstab(index=df_train[df_train['device.operatingSystem']\
+                                          .isin(df_train['device.operatingSystem']\
+                                                .value_counts()[:6].index.values)]['device.operatingSystem'], 
+                           
+                           # at this line, I am using the isin to select just the top 5 of browsers
+                           columns=df_train[df_train['device.browser'].isin(df_train['device.browser']\
+                                                                            .value_counts()[:5].index.values)]['device.browser'])
+# Ploting the crosstab that we did above
+crosstab_eda.plot(kind="bar",    # select the bar to plot the count of categoricals
+                 figsize=(14,7), # adjusting the size of graphs
+                 stacked=True)   # code to unstack 
+plt.title("Most frequent OS's by Browsers of users", fontsize=22) # adjusting title and fontsize
+plt.xlabel("Operational System Name", fontsize=19) # adjusting x label and fontsize
+plt.ylabel("Count OS", fontsize=19) # adjusting y label and fontsize
+plt.xticks(rotation=0) # Adjust the xticks, rotating the labels
+
+plt.show() # rendering
+
+
+
