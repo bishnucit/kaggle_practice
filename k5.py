033c59a10e3576d4daea63660a2af649dda25587
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2018-12-24 11:56:23 +0530

    Update k5.py

--- k5.py
+++ k5.py
@@ -7,7 +7,6 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
 import os
 print(os.listdir("../input"))
 
